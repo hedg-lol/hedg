a0b6cbbaf2ec6cebc60fc99bf3ed7577baa578a7
hedg-lol <197929680+hedg-lol@users.noreply.github.com>
2025-10-24 06:51:20 +0000

    feat: define protocol constants

---
+++ contracts/programs/hedg/src/constants.rs
@@ -0,0 +1,8 @@
+pub const MIN_COLLATERAL: u64 = 1_000_000_000;
+pub const SAFE_PERIOD: i64 = 86_400;
+pub const PROTOCOL_FEE_BPS: u64 = 50;
+pub const TRADE_FEE_BPS: u64 = 100;
+pub const DEPLOY_FEE: u64 = 20_000_000;
+pub const ESCROW_SEED: &[u8] = b"escrow";
+pub const CURVE_SEED: &[u8] = b"curve";
+pub const RECORD_SEED: &[u8] = b"record";
