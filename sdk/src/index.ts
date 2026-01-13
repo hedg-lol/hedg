b42fdcba169b8757fbad391299a65519e211de79
hedg-lol <197929680+hedg-lol@users.noreply.github.com>
2026-01-13 12:54:57 +0000

    style(sdk): organize exports alphabetically

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,2 +1,29 @@
-// HEDG Protocol SDK
-// Entry point - modules will be added as development progresses
+/**
+ * HEDG Protocol SDK
+ *
+ * TypeScript client for the HEDG on-chain program.
+ * Supports escrow management, bonding curve trading,
+ * and refund claim operations.
+ */
+
+export { HedgClient } from "./client";
+export type {
+  EscrowVault,
+  BondingCurve,
+  BuyerRecord,
+  TokenLaunch,
+  DeployerProfile,
+  TradeRecord,
+  ProtocolConfig,
+} from "./types";
+export {
+  MIN_COLLATERAL,
+  SAFE_PERIOD,
+  TRADE_FEE_BPS,
+  PROTOCOL_FEE_BPS,
+  DEPLOY_FEE,
+  PROGRAM_ID,
+  ESCROW_SEED,
+  CURVE_SEED,
+  RECORD_SEED,
+} from "./constants";
